e7d09f8ece035f771147b9b4f51efa4953933f3e
Briana Nzivu <bjnzivu@gmail.com>
2019-05-03 17:39:48 +0300

    st main,home and end xml

--- app/src/main/java/com/example/novel/home.java
+++ app/src/main/java/com/example/novel/home.java
@@ -1,7 +1,9 @@
 package com.example.novel;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class home extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+    public void gotopage1(View view) {
+        Intent Intent =new Intent (this,page1.class);
+        startActivity(Intent);
     }
 }
---
+++ app/src/main/java/com/example/novel/page2.java
@@ -0,0 +1,23 @@
+package com.example.novel;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class page2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page2);
+    }
+    public void gotopage3(View view) {
+        Intent Intent =new Intent (this,page6.class);
+        startActivity(Intent);
+    }
+    public void gotopage1(View view) {
+        Intent Intent =new Intent (this,page2.class);
+        startActivity(Intent);
+    }
+}
